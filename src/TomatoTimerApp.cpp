26c4b040464679a32b282cd3093a683a7a263a21
MelodicTechno <617179025@qq.com>
2026-01-28 20:23:29 +0800

    fix multiple windows bug

--- src/TomatoTimerApp.cpp
+++ src/TomatoTimerApp.cpp
@@ -9,6 +9,20 @@ CTomatoTimerApp theApp;
 
 BOOL CTomatoTimerApp::InitInstance()
 {
+    // Check for existing instance
+    HANDLE hMutex = CreateMutex(NULL, TRUE, L"TomatoTimer_Instance_Mutex");
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        CWnd* pWnd = CWnd::FindWindow(NULL, L"Tomato Timer");
+        if (pWnd)
+        {
+            // Restore window if minimized or hidden
+            pWnd->ShowWindow(SW_RESTORE);
+            pWnd->SetForegroundWindow();
+        }
+        return FALSE;
+    }
+
     CWinApp::InitInstance();
 
     AfxEnableControlContainer();
